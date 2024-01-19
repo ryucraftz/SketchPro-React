c552e33d9de0e6792cfe3130ac355a9a127294b2
ryucraftz <vaibhavsathe020>
2024-01-19 23:46:44 +0530

    first commit

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,7 +3,7 @@ import {create} from '@storybook/theming/create'
 
 const theme = create({
   base:'light',
-  brandTitle: 'react-native-sketchbook',
+  brandTitle: 'SketchPro-native',
   brandImage: 'https://user-images.githubusercontent.com/6936373/80469848-1df88e00-897c-11ea-8513-5b0b56eff55b.png'
 })
 
